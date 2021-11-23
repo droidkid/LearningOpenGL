4c7c0b895eb8f8876584e13faa1b8aead2ef6d66
yuvaraj <yuvaraj.chesetti@gmail.com>
2021-11-23 21:33:26 +0530

    Have a window up and running.

--- HelloTriangle/main.cpp
+++ HelloTriangle/main.cpp
@@ -4,5 +4,41 @@
 using namespace std;
 
 int main() {
-    cout<< "Hello, World!"<<endl;
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, 0);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Hello, Triangle", NULL, NULL);
+    if (window == NULL) {
+        cout<<"Failed to create GLFW window"<<endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwSetKeyCallback(window,
+                       [](GLFWwindow *window, int key, int scancode, int action, int mods) {
+        if (key == GLFW_KEY_ESCAPE && action==GLFW_PRESS) {
+            glfwSetWindowShouldClose(window, GLFW_TRUE);
+        }
+    });
+
+    glfwMakeContextCurrent(window);
+    gladLoadGL();
+    glfwSwapInterval(1);
+
+    glViewport(0, 0, 800, 600);
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    return 0;
 }
