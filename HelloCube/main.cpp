91936ad882c0fef3e56cd5a7f885fa6fa972c07c
yuvaraj <yuvaraj.chesetti@gmail.com>
2021-12-13 21:10:08 +0530

    Rotating the Cube.

--- HelloCube/main.cpp
+++ HelloCube/main.cpp
@@ -1,5 +1,7 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/ext.hpp>
 #include <iostream>
 
 using namespace std;
@@ -8,9 +10,11 @@ static const char* vertexShaderSource = R"(
 #version 460 core
 layout(location = 0) in vec3 aPos;
 layout(location = 1) in vec3 color;
+uniform mat4 mvp;
+
 out vec3 fragmentColor;
 void main() {
-    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+    gl_Position = mvp * vec4(aPos.x, aPos.y, aPos.z, 1.0);
     fragmentColor = color;
 }
 )";
@@ -31,7 +35,7 @@ int main() {
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_RESIZABLE, 0);
 
-    GLFWwindow* window = glfwCreateWindow(600, 600, "Hello, Rectangle", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(1024, 768, "Hello, Rectangle", NULL, NULL);
     if (window == NULL) {
         cout<<"Failed to create GLFW window"<<endl;
         glfwTerminate();
@@ -49,7 +53,7 @@ int main() {
     gladLoadGL();
     glfwSwapInterval(1);
 
-    glViewport(0, 0, 600, 600);
+    //glViewport(0, 0, 600, 600);
     glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 
     GLuint vertexShader;
@@ -119,81 +123,95 @@ int main() {
     }
 
     float colors[12*3*3] = {
-            0.0f, 0.5f, 0.5f,
-            0.0f, 0.5f, 0.5f,
-            0.0f, 0.5f, 0.5f,
+            1.0f, 0.0f, 0.0f,
+            1.0f, 0.0f, 0.0f,
+            1.0f, 0.0f, 0.0f,
+            1.0f, 0.0f, 0.0f,
+            1.0f, 0.0f, 0.0f,
+            1.0f, 0.0f, 0.0f,
 
-            0.5f, 0.5f, 0.5,
-            0.5f, 0.5f, 0.5,
-            0.5f, 0.5f, 0.5,
+            0.0f, 0.0f, 1.0f,
+            0.0f, 0.0f, 1.0f,
+            0.0f, 0.0f, 1.0f,
+            0.0f, 0.0f, 1.0f,
+            0.0f, 0.0f, 1.0f,
+            0.0f, 0.0f, 1.0f,
 
-            1.0f, 0.0f, 0.0,
-            0.0f, 1.0f, 0.0,
-            0.0f, 0.0f, 1.0,
+            0.0f, 1.0f, 1.0f,
+            0.0f, 1.0f, 1.0f,
+            0.0f, 1.0f, 1.0f,
+            0.0f, 1.0f, 1.0f,
+            0.0f, 1.0f, 1.0f,
+            0.0f, 1.0f, 1.0f,
 
-            0.5f, 0.5f, 0.5,
-            0.5f, 0.5f, 0.5,
-            0.5f, 0.5f, 0.5,
+            1.0f, 0.0f, 1.0f,
+            1.0f, 0.0f, 1.0f,
+            1.0f, 0.0f, 1.0f,
+            1.0f, 0.0f, 1.0f,
+            1.0f, 0.0f, 1.0f,
+            1.0f, 0.0f, 1.0f,
 
-            0.5f, 0.5f, 0.5,
-            0.5f, 0.5f, 0.5,
-            0.5f, 0.5f, 0.5,
+            0.0f, 1.0f, 0.0f,
+            0.0f, 1.0f, 0.0f,
+            0.0f, 1.0f, 0.0f,
+            0.0f, 1.0f, 0.0f,
+            0.0f, 1.0f, 0.0f,
+            0.0f, 1.0f, 0.0f,
 
-            0.5f, 0.5f, 0.5,
-            0.5f, 0.5f, 0.5,
-            0.5f, 0.5f, 0.0,
-
-            0.5f, 0.5f, 0.5,
-            0.5f, 0.5f, 0.5,
-            0.5f, 0.5f, 0.5,
-
-            0.5f, 0.5f, 0.5,
-            0.5f, 0.5f, 0.5,
-            0.5f, 0.5f, 0.5,
-            0.5f, 0.5f, 0.5,
-            0.5f, 0.5f, 0.5,
-            0.5f, 0.5f, 0.5,
-            0.5f, 0.5f, 0.5,
-            0.5f, 0.5f, 0.5,
-            0.5f, 0.5f, 0.5,
-            0.5f, 0.5f, 0.5,
-            0.5f, 0.5f, 0.5,
-            0.5f, 0.5f, 0.5,
-            0.5f, 0.5f, 0.5,
-            0.5f, 0.5f, 0.5,
-            0.5f, 0.5f, 0.5,
+            1.0f, 1.0f, 0.0f,
+            1.0f, 1.0f, 0.0f,
+            1.0f, 1.0f, 0.0f,
+            1.0f, 1.0f, 0.0f,
+            1.0f, 1.0f, 0.0f,
+            1.0f, 1.0f, 0.0f,
     };
 
-
+    // Bind Layout 0 attribute.
     GLuint vertexBuffer;
     glGenBuffers(1, &vertexBuffer);
     glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    // Bind Layout 0 attribute.
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*) 0);
     glEnableVertexAttribArray(0);
 
+    // Bind Layout 1 attribute.
     GLuint colorBuffer;
     glGenBuffers(1, &colorBuffer);
     glBindBuffer(GL_ARRAY_BUFFER, colorBuffer);
     glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
-    // Bind Layout 1 attribute.
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*) 0);
     glEnableVertexAttribArray(1);
 
+    glm::mat4 projection = glm::perspective(glm::radians(45.0f), 4.0f/3.0f, 0.1f, 100.0f);
+    glm::mat4 view = glm::lookAt(glm::vec3(4, 3, 3), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
+    glm::mat4 model = glm::mat4(1.0f);
+    glm::vec3 rotationAxis = glm::vec3(1.0, 0, 0);
+    glm::mat4 rotationMat = glm::mat4(1.0f);
+    rotationMat = glm::rotate( model, 45.0f, rotationAxis);
+
+    GLuint matrixId = glGetUniformLocation(shaderProgram, "mvp");
     glEnable(GL_DEPTH_TEST);
     glDepthFunc(GL_LESS);
 
+    glUseProgram(shaderProgram);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    rotationMat = glm::rotate( model, 360.0f * glm::sin((float)glfwGetTime() / 100.0f), rotationAxis);
+    glm::mat4 mvp = projection * view * rotationMat * model;
+    glUniformMatrix4fv(matrixId, 1, GL_FALSE, &mvp[0][0]);
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 6*2*3);
+    glfwSwapBuffers(window);
+
     while (!glfwWindowShouldClose(window)) {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); // Wireframe.
+        rotationMat = glm::rotate( model, 360.0f * glm::sin((float)glfwGetTime() / 100.0f), rotationAxis);
+        glm::mat4 mvp = projection * view * rotationMat * model;
+        glUniformMatrix4fv(matrixId, 1, GL_FALSE, &mvp[0][0]);
 
-        glUseProgram(shaderProgram);
-        glBindVertexArray(VAO);
-
-        //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); // Wireframe.
         glDrawArrays(GL_TRIANGLES, 0, 6*2*3);
-
         glfwSwapBuffers(window);
+
         glfwPollEvents();
     }
 
